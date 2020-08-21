d15aa1e6b339576c3431653e99198be1a16b6a2b
leoprogramer <37075245+leoprogramer@users.noreply.github.com>
2020-08-20 22:59:36 -0500

    primera subida
    toco por navegador

---
+++ behavior.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+	document.getElementById("smart_thumbnail");
+	var thumbnailElement = document.getElementById("smart_thumbnail");
+	var small = thumbnailElement.className = "";
+	
+	thumbnailElement.addEventListener("click", function(){
+  		
+  		
+});
+});
